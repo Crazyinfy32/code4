8bd7e57f7929f5a2b848229b2609d2d7925837e3
Crazyinfy32 <86730481+Crazyinfy32@users.noreply.github.com>
2021-09-03 23:15:13 -0400

    Add files via upload

---
+++ 源7.5.c
@@ -0,0 +1,33 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+int My_strlen(char* arr)
+{
+	int count = 0;
+    while (*arr != '\0')
+	{
+		count++;
+		arr++;
+	}
+	return count;
+}
+void reverse_string(char* arr)
+{
+	int left = 0;
+	int right = My_strlen(arr) - 1;
+	while (left < right)
+	{
+		char tmp = arr[left];
+		arr[left] = arr[right];
+		arr[right] = tmp;
+		left++;
+		right--;
+	}
+}
+int main()
+{
+	char arr[] = "abcdef";
+	reverse_string(arr);
+	printf("%s\n", arr);
+	return 0;
+
+}
